6f76353c69f1fd7814a662d315678c728755da18
Евгений <jonny153@mail.ru>
2018-09-30 20:10:16 +0200

    yandex verification

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <meta name="yandex-verification" content="98b92134968aa288" />
+
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
